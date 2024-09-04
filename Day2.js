7271b59aceed16cce2b1046713aa4cf8a1fc511f
ShubhamNayak14 <shubhamnayak446@gmail.com>
2024-09-04 11:25:40 +0530

    js

---
+++ Day2.js
@@ -0,0 +1,5 @@
+// generate random number using random
+
+var n = Math.random() * 100;
+n = Math.floor(n) + 1;
+console.log(n);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+ for (var i = 2; i > n; i++) {
+      output.push(output[output.lenght() - 2] + output[output.lenght() - 2]);
+    }
